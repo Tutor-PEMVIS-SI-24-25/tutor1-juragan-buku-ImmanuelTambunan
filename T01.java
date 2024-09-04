97da05c29d80f4a093929259cd8634320fab914f
ImmanuelTambunan <immannueltambunan@gmail.com>
2024-09-04 21:27:14 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,8 @@
-// NIM - NAMA
-// NIM - NAMA
+// NIM - NAMA 12S24034 - Immanuel Alexander Tambunan
+// NIM - NAMAimport java.util.*;
+import java.lang.Math;
 
+public class T01 {
+    public static void main(String[] args) {
+    }
+}
